82e156aee609a506cb556da9437ce2d4d6478d9b
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-09-02 16:41:08 -0400

    Kaitlyns test

--- db/index.js
+++ db/index.js
@@ -1,63 +1,79 @@
 // Connect to DB
 const { Client } = require('pg');
-const DB_NAME = 'localhost:5432/princesszelda'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
+const DB_NAME = 'localhost:5432/princesszelda';
+const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
+// HAHA HI KAITLYN
 
 // database methods
 
 // link table
-async function getAllLinks (){
+async function getAllLinks() {
   try {
     const { rows } = await client.query(`
     SELECT * 
     FROM link;
-    `)
+    `);
 
-    return rows
+    return rows;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
-async function getLinkById(id){
+async function getLinkById(id) {
   try {
-    const {rows: [ link ] } = await client.query(`
+    const {
+      rows: [link],
+    } = await client.query(
+      `
     SELECT * 
     FROM link
-    WHERE id = $1`, [ id ])
+    WHERE id = $1`,
+      [id]
+    );
 
-    return link
+    return link;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
-async function createLink ({url, comment}){
+async function createLink({ url, comment }) {
   try {
-    const { rows: [ link ]} = await client.query(`
+    const {
+      rows: [link],
+    } = await client.query(
+      `
     INSERT INTO link (url, comment)
     VALUES($1, $2)
-    RETURNING *;`, [url, comment])
+    RETURNING *;`,
+      [url, comment]
+    );
 
-    return link
+    return link;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 // tag table
 
-async function createTag ({tag_name}){
+async function createTag({ tag_name }) {
   try {
-    const { rows: [tags]} = client.query(`
+    const {
+      rows: [tags],
+    } = client.query(
+      `
     INSERT INTO tags (tag_name)
     VALUES ($1)
-    RETURNING *;`, [tag_name])
+    RETURNING *;`,
+      [tag_name]
+    );
 
-    return tags
+    return tags;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
@@ -67,6 +83,5 @@ module.exports = {
   getAllLinks,
   getLinkById,
   createLink,
-  createTag
-  
-}
+  createTag,
+};
