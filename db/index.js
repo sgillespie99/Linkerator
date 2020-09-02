8b53fe69d930c3a7d0ab4455c8c8ebb492032130
Sebastian <SebastianGillespie99@gmail.com>
2020-09-02 16:57:34 -0400

    sebastian test

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 const DB_NAME = 'localhost:5432/princesszelda';
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
-// HAHA HI KAITLYN
+// HAHA HI KAITLYN - Sebastian was here
 
 // database methods
 
