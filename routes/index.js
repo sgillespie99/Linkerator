243aa7274a8bc65a442e7f2c31edd79264d38333
Samatha-Fae Rapier <samathafae.rapier@gmail.com>
2020-09-06 21:48:58 -0400

    Fixed broken routes, added tag routes, tested all routes

--- routes/index.js
+++ routes/index.js
@@ -10,93 +10,140 @@ const { getAllLinks,
   getAllTags,
   createLinkTag,
   addTagsToLinks,
-  deleteTag} = require('../db')
+  deleteTag } = require('../db')
 
+// LINKS
 
-apiRouter.get("/links", (req, res, next)=> {
-  const links = await getAllLinks();
+apiRouter.get('/links', async (req, res, next) => {
+  try {
+    const links = await getAllLinks();
 
-  res.send({
-    links
-  })
-})
+    res.send({
+      links
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
-apiRouter.post('/links', async (req, res, next)=>{
+apiRouter.post('/links', async (req, res, next) => {
   const { url, comment, tags = "" } = req.body;
 
-  // This part is copied directly from Juicebox so I'm not 100% sure if its going to work
   const tagArr = tags.trim().split(/\s+/)
-
-
-  const postData = {}
+  const linkData = {};
 
   if (tagArr.length) {
-    postData.tags = tagArr;
+    linkData.tags = tagArr;
   }
 
   try {
-    postData.url = url
-    postData.comment = comment
+    linkData.url = url;
+    linkData.comment = comment;
 
-    const newLink = await createLink(postData)
+    const newLink = await createLink(linkData);
 
-    if (newLink){
-      res.send(newLink)
+    if (newLink) {
+      res.send(newLink);
     } else {
-      next ({
+      next({
         name: 'Link Creation Error',
         message: 'Error creating your link'
-      })
+      });
     }
-  } catch (error) {
-    throw error
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 })
 
-apiRouter.patch('/links/:id', async (req,res,next) => {
-  const { id } = req.params
-  const {url, comment, tags } = req.body;
+apiRouter.patch('/links/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { url, comment, tags } = req.body;
 
   const updateFields = {}
 
-  // all the tags stuff is copied so far
   if (tags && tags.length > 0) {
     updateFields.tags = tags.trim().split(/\s+/);
   }
 
-  if (url){
-    updateFields.url = url
+  if (url) {
+    updateFields.url = url;
   }
 
-  if (comment){
-    updateFields.comment = comment
+  if (comment) {
+    updateFields.comment = comment;
   }
 
   try {
-    
-    const updatedLink = await updateLink(id, updateFields)
-    // need to check if this is correct?
-    res.send({link: updatedLink})
-  } catch (error) {
-    throw error
+    const updatedLink = await updateLink(id, updateFields);
+    if (updatedLink) {
+      res.send({ link: updatedLink });
+    } else {
+      next({
+        name: 'Link Update Error',
+        message: ' An error occured while updating the link.'
+      });
+    }
+  } catch ({ name, message }) {
+    name({ name, message })
   }
-
 })
 
 
-apiRouter.delete('/links/:id', async (res, res, next) => {
-  
+apiRouter.delete('/links/:id', async (req, res, next) => {
   const { id } = req.params;
 
   try {
-    const deletedLink = await deleteLink(id)
-    res.send(deletedLink)
-  } catch (error) {
-    throw error
+    const link = await getLinkById(id);
+    if (link) {
+      const deletedLink = await deleteLink(link.id);
+
+      res.send({
+        message: deletedLink
+      });
+    } else {
+      next({
+        name: 'Error deleting link',
+        message: 'The link id does not exist!'
+      });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+// TAGS
+
+apiRouter.get('/tags', async (req, res, next) => {
+  try {
+    const tags = await getAllTags();
+
+    res.send({ tags });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+apiRouter.get('/:tagName/links', async (req, res, next) => {
+  const { tagName } = req.params;
+  try {
+    const links = await getLinksByTagName(tagName);
+
+    res.send({ links });
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 })
 
+apiRouter.delete('/tags/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedTag = await deleteTag(id);
 
+    res.send({ deletedTag });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+})
 
 apiRouter.get("/", (req, res, next) => {
   res.send({
@@ -109,3 +156,16 @@ apiRouter.use((error, req, res, next) => {
 });
 
 module.exports = apiRouter;
+
+// TESTING POSTS 
+
+// curl http://localhost:5000/api/links
+// curl http://localhost:5000/api/links -H "Content-Type: application/json" -X POST -d '{"url": "https://memegenerator.net/img/instances/75987203/youve-followed-a-broken-link.jpg", "comment": "Broken Link", "tags": "#meme #link #broken"}'
+// curl http://localhost:5000/api/links/14 -X PATCH -H 'Content-Type: application/json' -d '{"comment": "Broken Link", "tags": "#meme, #link, #broken, #funny"}'
+// curl http://localhost:5000/api/links/14 -X DELETE
+
+// TESTING TAGS
+
+// curl http://localhost:5000/api/tags
+// curl http://localhost:5000/api/%23zelda/links
+// curl http://localhost:5000/api/tags/28 -X DELETE
