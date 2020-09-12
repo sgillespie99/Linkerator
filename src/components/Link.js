ffbac0f216ee38cd73b826515cb4303171267149
Sebastian <SebastianGillespie99@gmail.com>
2020-09-12 15:14:12 -0400

    Links changes

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,28 +2,23 @@ import React from 'react';
 
 import './Link.css';
 
-const Link = ({
-    url,
-    comment,
-    click_count,
-    tags
-}) => {
-    return (
-        <div id="Link">
-            <div className="linkHead">
-                <p className="urlName">{url}</p>
-                <p className="clickCount">{click_count}</p>
-            </div>
-            <div className="linkBody">
-                <p className="linkComment">{comment}</p>
-                <p className="linkTags">{tags}</p>
-            </div>
-            <div className="linkUtils">
-                <button>Edit</button>
-                <button>Delete</button>
-            </div>
-        </div>
-    )
-}
+const Link = ({ url, comment, click_count, tags }) => {
+	return (
+		<div id="Link">
+			<div className="linkHead">
+				<p className="urlName">{url}</p>
+				<p className="clickCount">{click_count}</p>
+			</div>
+			<div className="linkBody">
+				<p className="linkComment">{comment}</p>
+				<p className="linkTags">{tags.map((t) => t.tag_name).join(' ')}</p>
+			</div>
+			<div className="linkUtils">
+				<button>Edit</button>
+				<button>Delete</button>
+			</div>
+		</div>
+	);
+};
 
 export default Link;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,31 +1,45 @@
 import React from 'react';
 
+import { getLinks } from '../api';
+
 import './Results.css';
 
 import Link from './Link';
 
+async function handleSubmit(event) {
+	event.preventDefault();
+}
+
+// const resultLinks = await getLinks({
+//     url,
+//     comment
+// })
+
 const Results = ({ results, linkList }) => {
-    // if (results) {
-    //     return (
-    //         <div className='resultsBlock'>
-    //             {
-    //                 results.map(result => (
-    //                     <Link key={result.id} {...result} />
-    //                 ))
-    //             }
-    //         </div>
-    //     )
-    // } else {
-    return (
-        <div className='resultsBlock'>
-            {
-                linkList.map(link => (
-                    <Link key={link.id} {...link} />
-                ))
-            }
-        </div>
-    )
-    // }
+	// if (results) {
+	//     return (
+	//         <div className='resultsBlock'>
+	//             {
+	//                 results.map(result => (
+	//                     <Link key={result.id} {...result} />
+	//                 ))
+	//             }
+	//         </div>
+	//     )
+	// } else {
+	console.log(linkList);
+	return (
+		<div className="resultsBlock">
+			<h1>
+				{' '}
+				Just some words
+				{linkList.map((link) => (
+					<Link key={link.id} {...link} />
+				))}
+			</h1>
+		</div>
+	);
+	// }
 };
 
 export default Results;
