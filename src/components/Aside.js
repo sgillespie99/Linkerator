0207a7066da88bf4d3cf8839da68b0691133486b
Samatha-Fae Rapier <samathafae.rapier@gmail.com>
2020-09-13 20:18:48 -0400

    Fixed CSS

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -52,10 +52,8 @@ const Aside = (props) => {
 					<input
 							type="text"
 							className="makeURL"
-							placeholder="Enter URL"
 							value={url}
 							onChange={handleUrl}>
-
 						</input>
 					</label>
 				</fieldset>
@@ -64,7 +62,6 @@ const Aside = (props) => {
 					<textarea
 							type="text"
 							className="makeComment"
-							placeholder="Enter comments"
 							value={comment}
 							onChange={handleComment}
 						></textarea>
@@ -75,7 +72,6 @@ const Aside = (props) => {
 					<textarea
 							type="text"
 							className="makeTags"
-							placeholder="Enter Tags"
 							value={tags}
 							onChange={handleTags}
 						></textarea>
@@ -83,7 +79,9 @@ const Aside = (props) => {
 				</fieldset>
 				<div>
 					<button type="submit" className="formBut" onClick={setLink}>
-						<img className="formBut" alt="navi-submit-button" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/854871cc-dca1-4f59-b4e9-8a8c697d0dd2/dcag3u4-82a658bc-2321-434d-8bf0-114f4ec80756.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvODU0ODcxY2MtZGNhMS00ZjU5LWI0ZTktOGE4YzY5N2QwZGQyXC9kY2FnM3U0LTgyYTY1OGJjLTIzMjEtNDM0ZC04YmYwLTExNGY0ZWM4MDc1Ni5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.m-ensK3n91aiMaApG2leu10h4Wzy-qAWiI8zX2dekAg" />
+						<span title="Submit">
+							<img className="formBut" alt="navi-submit-button" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/854871cc-dca1-4f59-b4e9-8a8c697d0dd2/dcag3u4-82a658bc-2321-434d-8bf0-114f4ec80756.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvODU0ODcxY2MtZGNhMS00ZjU5LWI0ZTktOGE4YzY5N2QwZGQyXC9kY2FnM3U0LTgyYTY1OGJjLTIzMjEtNDM0ZC04YmYwLTExNGY0ZWM4MDc1Ni5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.m-ensK3n91aiMaApG2leu10h4Wzy-qAWiI8zX2dekAg" />
+						</span>
 					</button>
 				</div>
 			</form>
--- src/components/Link.js
+++ src/components/Link.js
@@ -6,18 +6,30 @@ const Link = ({ url, comment, click_count, tags }) => {
 	return (
 		<div id="Link">
 			<div className="linkHead">
-				<a href={url} className="urlName">{url}</a>
-				<p className="clickCount">{click_count}</p>
+				<h4>URL:
+					<span> </span>
+					<a href={url} className="urlName">{url}</a>
+				</h4>
+				<span className="visited">
+					<h4>Visited:</h4>
+					<p className="clickCount">{click_count}</p>
+				</span>
 			</div>
 			<div className="linkContain">
-			<div className="linkBody">
-				<p className="linkComment">{comment}</p>
-				<p className="linkTags">{tags.map((t) => t.tag_name).join(' ')}</p>
-			</div>
-			<div className="linkUtils">
-				<button>Edit</button>
-				<button>Delete</button>
-			</div>
+				<div className="linkBody">
+					<div className="commentsBody">
+						<h4>Comments:</h4>
+						<p className="linkComment">{comment}</p>
+					</div>
+					<div className="tagsBody">
+						<h4>Tags:</h4>
+						<p className="linkTags">{tags.map((t) => t.tag_name).join(' ')}</p>
+					</div>
+				</div>
+				<div className="linkUtils">
+					<button className="editBut">EDIT</button>
+					<button className="deleteBut">DELETE</button>
+				</div>
 			</div>
 		</div>
 	);
